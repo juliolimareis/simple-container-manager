e09f95eab409a4f5d8bb4f5778a1bd1f7d5ceaa4
Julio Cesar Lima Reis <juliolimareis@gmail.com>
2023-02-25 20:31:34 -0300

    Fix: Added new colors in terminal

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+import { Box, Heading, } from "@chakra-ui/react";
+
 interface TerminalProps {
   container: Container,
   terminalLines: string[];
@@ -5,7 +8,49 @@ interface TerminalProps {
 
 const Terminal = ({ container, terminalLines }: TerminalProps): JSX.Element => {
   const erros = ["error", "errors"];
+  const warn = ["warn", "warnings", "warning"];
+
   const Line = (props: { line: string }): JSX.Element => {
     let color = "#04aa6d";
 
-export default Terminal
+    if(erros.some(e => props.line.includes(e))){
+      color = "#e96031";
+    }else if(warn.some(w => props.line.includes(w))){
+      color = "#e9e028";
+    }
+
+    return <Box as='p' color={color}>{props.line}</Box>;
+  };
+
+  return (
+    <Box>
+      <Heading as="h3"
+        p={3}
+        fontSize={20}
+      >
+        Tail Logs - <Box as="b" color="#04aa6d">{container?.name}</Box>
+      </Heading>
+
+      <Box
+        p={7}
+        h={300}
+        maxW={1200}
+        margin='auto'
+        overflow='auto'
+        textAlign='left'
+        backgroundColor='black'
+        border='solid 2px #5d8bbf'
+        borderRadius={20}
+        color='#04aa6d'
+      >
+        {
+          terminalLines.map((line, i) => (
+            <Line line={line} key={i}/>
+          ))
+        }
+      </Box>
+    </Box>
+  );
+};
+
+export default Terminal;
